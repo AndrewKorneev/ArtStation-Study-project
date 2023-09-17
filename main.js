e1198498a238734c45df5cfabad6368823f92435
Андрей <144910320+AndrewKorneev@users.noreply.github.com>
2023-09-17 21:12:03 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
 console.info("JS успешно подключен!");
 
-document.getElementsByClassName("headerContent").onclick = function(event) {
-  var target = event.target;
-  if (target.className == "genreMenu") {
-    var s = target.getElementByClassName ("subMenu");
-    closeMenu();
-    s[0].style.display = "block";
-  }
+function checkEvent () {
+  var target = document.querySelector(".headerContent");
+  var s = target.querySelector (".subMenu");
+  s.style.display = "block";
 }
+
+const genreMenu = document.querySelector(".genreMenu");
+genreMenu.addEventListener('click', checkEvent);
+
+
 
 document.onclick = function (event) {
   var target = event.target;
-  console.log (event.target);
   if (target.className != "genreMenu" && target.className != "subMenu") {
     closeMenu();
   } 
 }
 
 function closeMenu() {
-  var menu = document.getElementsByClassName ("headerContent");
   var subm = document.getElementsByClassName("subMenu");
   for (var i=0; i<subm.length; i++) {
     subm[i].style.display = "none";
@@ -26,28 +26,5 @@ function closeMenu() {
 }
 
 
-/*
-function sum (arr) {
-var res = 0;
-for (var i = 0; i < arr.length; i++) {
-  res += arr[i];
-}
-return res;
-//переменная внутри функции становится локальной, только если определена 
-//через var или let. если обьявить ее просто так, она будет глобальной 
-}
 
-if (confirm ("Ввести количесвто элементов массива для сложения?")) {
-var leng = Number (prompt ("Введите размер массива!"));
-var i = 0;
-let array = [];
-for (leng; leng > 0; leng--) {
-array [i] = Number (prompt("Элемент " + (i + 1)));
-i++;
-}
-alert ("Сумма чисел: " + sum(array));
-} else {
-  alert ("Отменено!");
-}
-*/
 
